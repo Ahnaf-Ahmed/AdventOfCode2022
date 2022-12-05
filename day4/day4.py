e69f3d949f241f656b6a7f0cb3385e57d55a56b9
Ahnaf Ahmed <nemba246@gmail.com>
2022-12-04 20:14:13 -0800

    differentiated part 1 and 2

--- day4/day4.py
+++ day4/day4.py
@@ -1,5 +1,21 @@
-def overlap(a, b):
-    return a[0] <= b[1] and b[0] <= a[1]
+# part 1
+
+# overlap = 0
+# with open('input.txt') as f:
+#     line = f.readline()
+#     while line:
+#         [one,two] = line.split(',')
+        
+#         one = [int(x) for x in one.split('-')]
+#         two = [int(x) for x in two.split('-')]
+        
+#         if (one[0] <= two[0] and one[1] >= two[1]) or (two[0] <= one[0] and two[1] >= one[1]):
+#             overlap += 1
+#         line = f.readline()
+# print(f"overlap is {overlap}")
+
+
+# part 2
 
 overlap = 0
 with open('input.txt') as f:
